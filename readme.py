9b19b3307f113a2cfc73e868acca934746b417e9
Allusura <110790823+Allusura@users.noreply.github.com>
2024-01-16 18:29:58 +0000

    Create readme.py

---
+++ readme.py
@@ -0,0 +1,12 @@
+#Description:
+# A program which returns the Nato Alphabet
+# for the word that you input
+# i.e input = "Andy"
+# i.e return = "Alpha, November, Delta, Yankee"
+
+#Things I learned today:
+# List Comprehension (  new_list = [new_item for item in list]  )
+# Works with Strings too i.e ("Andy" = "A", "n", "d", "y")
+# Conditional List Comprehension (  new_list = [new_item for item in list if test]  )
+# Dictionary Comprehension
+# How to Iterate over a Pandas DataFrame
